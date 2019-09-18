b9244d9b1a0bff1938dec59818fda4be11cf1b6b
Zenobia <lan13ji@163.com>
2019-09-18 14:55:30 +0800

    黑马头条PC-登录页布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="login">
-    <!-- 使用elemnetUI组件 -->
+    <!-- 使用elemnetUI组件 el-card -->
     <el-card class="login-card">
+      <!-- 匿名插槽 -->
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
       <!-- 表单，用el-form包裹 -->
-      <el-form>
-          <!-- 每一个表单域用el-form-item包裹 -->
-        <el-form-item>
-            <!--  -->
-          <el-input></el-input>
+      <!-- 数据校验 先绑定 model属性,rules规则 -->
+      <el-form :model="loginForm" :rules="loginRules" ref="myForm" style="margin-top:20px">
+          <!-- 每一个表单域由一个 Form-item 组件构成 -->
+          <!-- form-item prop属性绑定 表单组件的 校验字段名 -->
+        <el-form-item prop="mobile">
+          <!-- 表单域中可以放置各种类型的表单控件，包括Input、Select、Checkbox -->
+          <!-- 手机号 绑定 v-model -->
+          <el-input placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>
         </el-form-item>
-        <el-form-item>
-            <!-- 验证码 -->
-          <el-input style="width:65%"></el-input>
+
+        <!-- 验证码 -->
+        <el-form-item prop="code">
+          <el-input placeholder="请输入验证码" style="width:65%" v-model="loginForm.code"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <!-- 同意选项 -->
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+
+        <!-- 同意选项 -->
+        <el-form-item prop="agree">
+          <el-checkbox v-model="loginForm.agree">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
+
+        <!-- 登录按钮 -->
         <el-form-item>
-            <!-- 登录按钮 -->
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button type="primary" style="width:100%" @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -31,7 +38,62 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      // rule当前规则
+      // value当前表单项的值
+      // callBack当前回调函数
+      if (value) {
+        callBack()
+      } else {
+        callBack(new Error('请您同意协议'))
+      }
+    }
+    return {
+      // 表单数据 是一个对象
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        agree: false // 是否同意协议
+      },
+      // 登录规则集合对象
+      loginRules: {
+        // 决定校验规则， key(字段名):value(对象数组)，一个对象就是一个校验规则
+        mobile: [
+          {
+            required: true, // true 表示该字段必填，如果没有内容，就会提示消息
+            message: '请输入您的手机号'
+          },
+          { pattern: /^1[3456789]\d{9}$/,
+            message: '请输入正确的手机号'
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '请输入您的验证码'
+          }, {
+            pattern: /^\d{6}$/,
+            message: '验证码位6位数字'
+          }
+        ],
+        agree: [{ validator }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 校验整个表单规则
+      // validate 是一个方法，传入的是一个回调函数，函数中有两个参数，分别是是否校验成功/未校验成功的字段
+      this.$refs.myForm.validate(isOk => {
+        if (isOk) {
+          console.log('校验成功')
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -46,7 +108,7 @@ export default {}
   background-size: cover;
   .login-card {
     width: 440px;
-    height: 330px;
+    height: 340px;
     .title {
         text-align: center;
         img {

d4c8957385e07462d102c91102cbf8182e76bbe8
Zenobia <lan13ji@163.com>
2019-09-21 09:35:23 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- separator 是分隔符 -->
+  <el-breadcrumb separator=">">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>
+      <a href="/">
+        <!-- 定义一个插槽 -->
+        <slot name="title"></slot>
+      </a>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import layoutAside from './home/layout-aside'
 import layoutHeade from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 export default {
   // 调用install的时候传入了Vue
   install: function (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-heade', layoutHeade)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }

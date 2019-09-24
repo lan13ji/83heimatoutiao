be3ae233a34a36ea9b7669163fae26521895c204
Zenobia <lan13ji@163.com>
2019-09-24 21:53:16 +0800

    发表文章-封面图片选择，自定义上传

--- src/components/publish/select-image.vue
+++ src/components/publish/select-image.vue
@@ -1,27 +1,27 @@
 <template>
   <el-tabs>
     <el-tab-pane label="素材库">
-      <!-- 全部不素材数据 -->
+      <!-- 全部素材数据 -->
       <div class="imgs-list">
-          <el-card class="imgs-item" v-for="item in list" :key="item.id">
-              <img :src="item.url" alt="" @click="clickImg(item)">
-          </el-card>
+        <el-card class="imgs-item" v-for="item in list" :key="item.id">
+          <img :src="item.url" alt @click="clickImg(item.url)" />
+        </el-card>
       </div>
       <el-row type="flex" justify="center">
-          <el-pagination
-            background
-            layout="prev,pager,next"
-            :total="page.total"
-            :current-page="page.currentPage"
-            :page-size="page.pageSize"
-            @current-change="changePage"></el-pagination>
+        <el-pagination
+          background
+          layout="prev,pager,next"
+          :total="page.total"
+          :current-page="page.currentPage"
+          :page-size="page.pageSize"
+          @current-change="changePage"
+        ></el-pagination>
       </el-row>
     </el-tab-pane>
     <el-tab-pane label="上传图片">
-        <el-upload class="avatar-uploader" :show-file-list="false" action="">
-            <i class="el-icon-plus avatar-uploader-icon"></i>
-        </el-upload>
-
+      <el-upload class="avatar-uploader" :show-file-list="false" action="" :http-request="uploadImg">
+        <i class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
     </el-tab-pane>
   </el-tabs>
 </template>
@@ -39,7 +39,9 @@ export default {
     }
   },
   methods: {
+    //  上传用户素材
     uploadImg (params) {
+      console.log(params)
       let data = new FormData()
       data.append('image', params.file)
       this.$axios({
@@ -47,11 +49,12 @@ export default {
         method: 'POST',
         data
       }).then(result => {
+        // 得到一个线上地址
         this.$emit('selectOne', result.data.url)
       })
     },
-    clickImg (item) {
-      this.$emit('selectOne', item.url)
+    clickImg (url) {
+      this.$emit('selectOne', url)
     },
     changePage (newPage) {
       this.page.currentPage = newPage
@@ -65,7 +68,7 @@ export default {
           page: this.page.currentPage,
           per_page: this.page.pageSize
         }
-      }).then((result) => {
+      }).then(result => {
         this.list = result.data.results
         this.page.total = result.data.total_count
       })
@@ -78,44 +81,44 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    .avatar-uploader{
-        width:178px;
-        position: relative;
-        border: 1px dashed #d9d9d9;
-        border-radius: 6px;
-        cursor: pointer;
-        overflow: hidden;
+.avatar-uploader {
+  width: 178px;
+  position: relative;
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  overflow: hidden;
 
-        &:hover{
-            border-color: #409EFF;
-        }
-        .avatar-uploader-icon {
-            width: 178px;
-            height: 178px;
-            line-height: 178px;
-            text-align: center;
-            font-size: 28px;
-            color: #8c939d;
-        }
+  &:hover {
+    border-color: #409eff;
+  }
+  .avatar-uploader-icon {
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+    font-size: 28px;
+    color: #8c939d;
+  }
+}
+.imgs-list {
+  display: flex;
+  flex-wrap: wrap;
+
+  .imgs-item {
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+
+    .el-card__body {
+      padding: 0;
+
+      img {
+        width: 100%;
+        height: 100%;
+        display: block;
+      }
     }
-    .imgs-list{
-        display:flex;
-        flex-wrap:wrap;
-
-        .imgs-item{
-            width: 100px;
-            height: 100px;
-            margin: 10px;
-
-            .el-card__body{
-                padding: 0;
-
-                img{
-                    width: 100%;
-                    height: 100%;
-                    display: block;
-                }
-            }
-        }
-    }
+  }
+}
 </style>

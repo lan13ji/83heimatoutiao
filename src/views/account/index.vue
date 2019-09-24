5804e3592d0bf32c7fda8b27044fd66c6ce10e4a
Zenobia <lan13ji@163.com>
2019-09-24 17:06:53 +0800

    账户信息-用户信息获取

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,28 +5,45 @@
         </bread-crumb>
         <el-form label-width="120px">
             <el-form-item label="用户名">
-                <el-input style="width: 300px;"></el-input>
+                <el-input v-model="formData.name" style="width: 300px;"></el-input>
             </el-form-item>
             <el-form-item label="简介">
-                <el-input style="width: 300px;"></el-input>
+                <el-input v-model="formData.info" style="width: 300px;"></el-input>
             </el-form-item>
             <el-form-item label="邮箱">
-                <el-input style="width: 300px;"></el-input>
+                <el-input v-model="formData.email" style="width: 300px;"></el-input>
             </el-form-item>
             <el-form-item label="手机号">
-                <el-input style="width: 300px;"></el-input>
+                <el-input disabled v-model="formData.mobile" style="width: 300px;"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type='primary'>保存信息</el-button>
             </el-form-item>
         </el-form>
-        <img class="user-img" src="../../assets/img/404.png" alt="">
+        <img class="user-img" :src="formData.photo? formData.photo : defaultImg" alt="">
     </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      defaultImg: require('../../assets/img/404.png'),
+      formData: {}
+    }
+  },
+  methods: {
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
